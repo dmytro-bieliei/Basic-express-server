84d12be309c1dca6676093b1dcfc76a22971c13f
dmytro-bieliei <dmbieliei@gmail.com>
2022-12-23 13:24:43 +0200

    add models

---
+++ models/Book.js
@@ -0,0 +1,18 @@
+class Book {
+  static bookDB = [
+    {
+      id: 1,
+      title: 'Head First JavaScript programming',
+      authors:'Elisabeth Robson, Eric Freeman',
+      publicationDate: 'may 6, 2014',
+    },
+    {
+      id: 2,
+      title: 'Begining ReactJS foundations',
+      authors:'Chris Minnick',
+      publicationDate: 'february 11, 2022',
+    },
+  ];
+}
+
+module.exports = Book;
